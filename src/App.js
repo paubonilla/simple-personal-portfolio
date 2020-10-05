17ae8f6a89682b4d912931c29e4c954171a9a25d
Pau Bonilla <paulo111995@gmail.com>
2020-10-04 21:18:46 -0700

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom'
 import Nav from './components/layout/Nav/Nav';
@@ -9,26 +10,46 @@ import Projects from './components/pages/Projects/Projects';
 import Blog from './components/pages/Blog';
 import About from './components/pages/About/About';
 import Gallery from './components/pages/Gallery/Gallery';
-import styled from 'styled-components'
+import { CubeGrid } from 'styled-loaders-react';
+import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   width: 100vw;
 `
 
+export const Inner = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => { setLoading(false); }, 3000)
+  }, [])
   return (
-    <AppContainer>
-      <Nav />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/portfolio' component={Portfolio} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/blog' component={Blog} />
-        <Route path='/about' component={About} />
-        <Route path='/gallery' component={Gallery} />
-      </Switch>
-      {/* <Footer /> */}
-    </AppContainer>
+    <>
+      {loading ?
+        <Inner>
+          <CubeGrid color="#f1f3f8" size="70px" />
+        </Inner>
+        :
+        <AppContainer>
+          <Nav />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/blog' component={Blog} />
+            <Route path='/about' component={About} />
+            <Route path='/gallery' component={Gallery} />
+          </Switch>
+          {/* <Footer /> */}
+        </AppContainer>}
+    </>
   );
 }
 
