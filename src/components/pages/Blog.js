dd5835aa88778d4aaf59868187cc667ff80205cc
Pau Bonilla <paulo111995@gmail.com>
2022-04-16 22:15:03 +0300

    hi

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -7,7 +7,7 @@ export default function Blog() {
       <h1 className="blog">
         Blog Page
 
-        <p>Lead is a chemical element with the symbol Pb and atomic number 82. It is a heavy metal that is denser than most common materials. Lead is soft and malleable, and also has a relatively low melting point. When freshly cut, lead is silvery with a hint of blue; it tarnishes to a dull gray color when exposed to air.</p>
+        <p>about me</p>
       </h1>
     </>
   )
